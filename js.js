6dd923d99d03d7c9a49bea18bde9326c0dd5b0ad
Yehor-Somov <yehor.somov@gmail.com>
2021-03-19 19:54:23 +0200

    in process

--- js.js
+++ js.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-	$(".dial").knob();
+	$(".taho").knob({
+		'angleArc': 250,
+		'angleOffset': -125,
+		'min' : 0,
+		'max' : 8000
+	});
+
+	$(".speed").knob({
+		'angleArc': 250,
+		'angleOffset': -125,
+		'min' : 0,
+		'max' : 220,
+		'width': 300,
+		'height': 300
+	});
+
+	$(".benz").knob({
+		'angleArc': 90,
+		'angleOffset': -10,
+		'min' : 0,
+		'max' : 40,
+		'width': 100,
+		'height': 100
+	});
 });
