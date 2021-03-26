dec23c78425e3865e94b79f0aade8d98a7435a04
Yehor-Somov <yehor.somov@gmail.com>
2021-03-26 19:55:17 +0200

    for Rakushki

--- js.js
+++ js.js
@@ -1,46 +1,37 @@
 $(function() {
-    $(".hour").knob({
+    $("#R").knob({
         'min': 0,
-        'max': 24,
+        'max': 255,
         'bgColor' : '#333',
-        'fgColor' : '#ffec03', 
+        'fgColor' : 'rgb(255, 0, 0)', 
         'displayInput' : false,
         'width' : 300,
         'height' : 300,
         'thickness': 0.3
     });
-    $(".minute").knob({
+    $("#G").knob({
         'min': 0,
-        'max': 60,
+        'max': 255,
         'bgColor' : '#333',
+        'fgColor' : 'rgb(0, 255, 0)',
         'displayInput' : false,
-        'width' : 200,
-        'height' : 200,
-        'thickness': 0.45
+        'width' : 300,
+        'height' : 300,
+        'thickness': 0.3
     });
-    $(".second").knob({
+    $("#B").knob({
         'min': 0,
-        'max': 60,
+        'max': 255,
         'bgColor' : '#333',
-        'fgColor' : 'rgb(127, 255, 0)', 
+        'fgColor' : 'rgb(0, 0, 255)', 
         'displayInput' : false,
-        'width' : 100,
-        'height' : 100,
+        'width' : 300,
+        'height' : 300,
         'thickness': 0.3
     });
 
+    setInterval(() => $('.container').css({
+        'background' : `rgb(${$("#R").val()}, ${$("#G").val()}, ${$("#B").val()})`
+    }), 1);
+
 });
-function clock() {
-    var $s = $(".second"),
-    $m = $(".minute"),
-    $h = $(".hour");
-    d = new Date(),
-    s = d.getSeconds(),
-    m = d.getMinutes(),
-    h = d.getHours();
-    $s.val(s).trigger("change");
-    $m.val(m).trigger("change");
-    $h.val(h).trigger("change");
-    setTimeout("clock()", 1000);
-}
-clock();
